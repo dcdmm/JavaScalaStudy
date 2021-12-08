29624a2ed5f713667756a5332ac0390ca7bb8001
dcdmm <dcdmmcomeon@gmail.com>
2021-12-08 22:05:12 +0800

    明天继续加油完成集合部分的学习.马上就要搞定scala了.哈哈.舒服.不过,本来应该早就搞定的.哎

--- src/main/scala/sd_container/function_/math0.scala
+++ src/main/scala/sd_container/function_/math0.scala
@@ -1,10 +1,12 @@
 package sd_container.function_
 
+import scala.collection.TraversableOnce
+
 object math0 {
     def main(args: Array[String]): Unit = {
         val list = List(1, 3, 4, -2, 9, 10, 7)
         val list1 = List(("a", 1), ("b", 3), ("c", 4), ("d", -2), ("e", 9), ("g", 10), ("h", 7))
-
+        TraversableOnce
         // 求和
         println(list.sum)
         // 求乘积
