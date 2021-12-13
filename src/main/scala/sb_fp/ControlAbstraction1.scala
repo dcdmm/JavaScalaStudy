a55f2cfdcc9821b6dd39cb74797495af411f03ac
dcdmm <dcdmmcomeon@gmail.com>
2021-12-13 15:18:15 +0800

    不要太纠结那些不那么重要的事

--- src/main/scala/sb_fp/ControlAbstraction1.scala
+++ src/main/scala/sb_fp/ControlAbstraction1.scala
@@ -37,7 +37,7 @@ object ControlAbstraction1 {
         // 匿名函数的写法;与函数myWhile等价
         def myWhile1(condition: => Boolean): (=> Unit) => Unit = {
             // (op: => Unit) => {......}  // 报错
-            // op表示代码块时,必须省略op的数据类型
+            // op表示代码块时,匿名函数的参数类型必须省略
             op => {
                 if (condition) {
                     op
