0ee180dded7ebce4076ea054b3d40efcd422d353
dcdmm <dcdmmcomeon@gmail.com>
2023-02-01 10:34:29 +0800

    小优化

--- src/main/java/a_basic/ArithmeticOperator.java
+++ src/main/java/a_basic/ArithmeticOperator.java
@@ -29,5 +29,18 @@ public class ArithmeticOperator {
         // 运算顺序从左至右;
         System.out.println("hello" + 100 + 200);  // 输出"hello100200"
         System.out.println(100 + 200 + "hello");  // 输出"300hello"
+
+        // +=,-=,/=,*=,%=;(自动进行强制类型转换)
+        a += 20.34; // 等价于a = (int)(a + 20.34)
+        System.out.println(a);
+        System.out.println("***************************");
+
+        short s = 2;
+        // s = s + 1;  // 报错:1是int类型数据,故s+1也是int类型数据
+        /*
+        解决方法如下:
+        法1, s = (short)(s+1);
+        法2, s += 1;
+        */
     }
 }
